06f4ceb5fb4dabd9cedaba9d96f5906a65cc757a
Magnus Hjelm <magnus.hjelm@gmail.com>
2011-01-02 09:29:02 +0100

    first commit

---
+++ laterthis.js
@@ -0,0 +1,21 @@
+var baseUrl = "http://laterthis.com/post?stand_alone=true";
+var url;
+var title;
+var selection 
+
+function save() {
+	chrome.tabs.getSelected(null , function(tab) {
+		url = (tab.url);
+ 		title = (tab.title);
+		chrome.tabs.sendRequest(tab.id, {method: "getSelection"}, function (response) {
+		  selection = (response.data);
+			chrome.tabs.update(tab.id, {url: baseUrl + '&link_url=' + encodeURIComponent(url) + '&link_title=' + encodeURIComponent(title) + '&link_comment=' + encodeURIComponent(selection)});
+		});
+	});
+}
+
+function goto() {
+	chrome.tabs.getSelected(null , function(tab) {
+		chrome.tabs.update(tab.id, {url: 'http://laterthis.com/my_links/'});
+	});
+}
